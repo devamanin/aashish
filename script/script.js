f4b000ced8932ae92e98bdbc727e519d9bb0daed
devamanin <amansingh1032000@gmail.com>
2023-08-11 15:50:00 +0530

    Updated billing page

--- script/script.js
+++ script/script.js
@@ -5,8 +5,11 @@ document.getElementsByClassName("mobile")[0].addEventListener("click", function(
     document.getElementsByClassName("premium")[0].classList = "premium option-cn"
 
 
-    let ls = document.getElementsByTagName("tbody").getElementsByTagName("tr");
+    let ls = document.querySelectorAll("tbody tr");
     console.log(ls);
+    // for (let i = 0; i< ls.length; i++){
+    //     console.log(ls[i]);
+    // }
     // let trlist = ls.document.getElementsByTagName("tr")[0];
 })
 document.getElementsByClassName("basic")[0].addEventListener("click", function(){
@@ -26,4 +29,15 @@ document.getElementsByClassName("premium")[0].addEventListener("click", function
     document.getElementsByClassName("mobile")[0].classList = "mobile option-cn"
     document.getElementsByClassName("standard")[0].classList = "standard option-cn"
     document.getElementsByClassName("basic")[0].classList = "basic option-cn"
+})
+
+
+document.getElementById("color_mode").addEventListener("click", function(){
+    let checkbox = document.getElementById("color_mode");
+    if (checkbox.checked == false){
+        console.log("true");
+    }
+    else{
+        console.log("asd")
+    }
 })
